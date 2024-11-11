16a0aeff7217584bbf95aa5eeda778d8144eb03b
Radevac <radevac000@gmail.com>
2024-11-11 15:04:27 +0200

    Hom 13 Cars

---
+++ ASP/Dolly/Toyota_Cars/Toyota_Cars/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationGeo.Models.Cars.Toyota;
+
+namespace Toyota_Cars.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<ToyotaModel> ToyotaModels { get; set; }
+    
+    public DbSet<ConfigurationModel> Configurations { get; set; }
+    
+    public DbSet<ConfigurationColorsModel> ConfigurationColors { get; set; }
+    
+    public DbSet<ColorModel> Colors { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
